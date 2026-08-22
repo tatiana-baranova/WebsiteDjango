de5ea86341d0e4f639d8c73a21e76817f2f5a672
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-22 16:01:52 +0300

    add courses model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Course(models.Model):
+    slug = models.SlugField('Унікальна назва курсу')
+    title = models.CharField('Назва курса', max_length=120)
+    description = models.TextField('Опис курсу')
+    image = models.ImageField('Зображення', default='default.png', upload_to='courses_images')
 
-# Create your models here.
+    def __str__(self):
+        return self.title
+
+    # class Meta:
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,17 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Course
+
+
+class HomePage(ListView):
+    model = Course
+    template_name = 'home.html'
+    context_object_name = 'course'
+    ordering = ['-id']
+
+    def get_context_data(
+        self, *, object_list = ..., **kwargs):
+        ctx = super(HomePage, self).get_context_data(**kwargs)
+        ctx['title'] = 'Головна сторінка'
+        return ctx
